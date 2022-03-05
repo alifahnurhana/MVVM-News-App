298f55f569e95063e9b519fbbc3f18516be9a087
alifahnurhana <alifahnur2609@gmail.com>
2022-03-05 23:46:57 +0800

    init commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/Convertesrs.kt
@@ -0,0 +1,19 @@
+package com.androiddevs.mvvmnewsapp.db
+
+import androidx.room.TypeConverter
+import com.androiddevs.mvvmnewsapp.model.Source
+
+
+class Convertesrs {
+
+    @TypeConverter
+    fun fromSource(source: Source) : String {
+        return source.name
+
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "75cc4db8d1934975b596a9440db93c7e"
+        const val BASE_URL = "https://newsap1.org"
+    }
+}
